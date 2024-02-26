a5313bcd72cc69fcf7f0381af15becc49ccf6499
khangar-harish <khanhash1992@gmail.com>
2024-02-26 14:29:37 +0000

    cd for project

--- src/main/java/com/hash/cicddemo/CicdDemoApplication.java
+++ src/main/java/com/hash/cicddemo/CicdDemoApplication.java
@@ -18,7 +18,7 @@ public class CicdDemoApplication {
 	}
 
 	public static void main(String[] args) {
-		logger.info("Application executed...");
+		logger.info("Application executed***...");
 		SpringApplication.run(CicdDemoApplication.class, args);
 	}
 
